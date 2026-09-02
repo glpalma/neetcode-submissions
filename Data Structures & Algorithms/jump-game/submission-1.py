ec5381158abaff7893c5f1491af669f2412730bb
Gustavo Palma <70819809+glpalma@users.noreply.github.com>
2026-09-02 10:05:19 -0300

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        reachable = 0
+
+        for i in range(n):
+
+            if reachable >= i:
+                reachable = max(reachable, i + nums[i])
+
+            if reachable >= n-1:
+                return True
+
+        return False
+
