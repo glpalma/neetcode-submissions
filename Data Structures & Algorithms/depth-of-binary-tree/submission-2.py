ae83c22d39d1b45122fb706b4339b9709966b4f5
Gustavo Palma <70819809+glpalma@users.noreply.github.com>
2026-09-05 16:01:05 -0300

    Add: depth-of-binary-tree - submission-2

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-2.py
@@ -0,0 +1,55 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    # Recursion DFS
+    # def maxDepth(self, root: Optional[TreeNode]) -> int:
+    #     if not root:
+    #         return 0
+        
+    #     return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+    # Stack DFS
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        q = deque([(root, 1)])
+        
+        maxd = 0
+
+        while q:
+            node, depth = q.pop()
+            maxd = max(maxd, depth)
+
+            if node.left:
+                q.append((node.left, depth + 1))
+            if node.right:
+                q.append((node.right, depth + 1))
+
+
+        return maxd
+
+    # BFS
+    # def maxDepth(self, root: Optional[TreeNode]) -> int:
+    #     q = deque()
+    #     if root:
+    #         q.append(root)
+
+    #     level = 0
+    #     while q:
+    #         for i in range(len(q)):
+    #             node = q.popleft()
+    #             if node.left:
+    #                 q.append(node.left)
+    #             if node.right:
+    #                 q.append(node.right)
+    #         level += 1
+    #     return level
+
+
+        
